945cc10b0b41da635ce5c8f6f584f6894db0c231
Igor Santos <santos.igor1@escolar.ifrn.edu.br>
2023-11-03 13:21:56 -0300

    mudanças de novo

--- testes VecArray/geradordetestes.py
+++
@@ -1,8 +0,0 @@
-from random import randint as rd
-for i in range (1, 101):
-    f = open(f'teste{i:02d}.txt', 'w')
-
-    f.write(f"{i*100}\n")
-    for j in range(i*100):
-        f.write(f"{i}\n")
-print("DONE")
